079d3ca9426bf9784f6fc0bf84a0ef58b50f052b
Nicolás San Martin <nicolas.sm.lor@gmail.com>
2024-07-14 10:50:05 -0400

    se añade funcion de adicion y sustraccion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,39 @@
-precio = 400000
+basePrice = 400000;
+totalPrice = 0;
+baseQuantity = 0;
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+priceSpan = document.querySelector("#basePriceId");
+priceSpan.innerHTML = basePrice;
+
+quantitySpan = document.querySelector("#quantityId");
+quantitySpan.innerHTML = baseQuantity;
+
+totalPriceSpan = document.querySelector("#totalPriceId");
+totalPriceSpan.innerHTML = totalPrice;
+
+const addition = () => {
+  let quantityAddition = document.querySelector("#quantityId");
+  let contentQuantity = Number(quantityAddition.innerHTML);
+  contentQuantity = contentQuantity + 1;
+  quantityAddition.innerHTML = contentQuantity;
+
+  let totalPriceAddition = document.querySelector("#totalPriceId");
+  let contentTotalPrice = Number(totalPriceAddition.innerHTML);
+  contentTotalPrice = contentTotalPrice + basePrice;
+  totalPriceAddition.innerHTML = contentTotalPrice;
+};
+
+const subtraction = () => {
+  let quantitySubtraction = document.querySelector("#quantityId");
+  let contentQuantity = Number(quantitySubtraction.innerHTML);
+
+  if (contentQuantity > 0) {
+    contentQuantity = contentQuantity - 1;
+    quantitySubtraction.innerHTML = contentQuantity;
+
+    let totalPriceSubtraction = document.querySelector("#totalPriceId");
+    let contentTotalPrice = Number(totalPriceSubtraction.innerHTML);
+    contentTotalPrice = contentTotalPrice - basePrice;
+    totalPriceSubtraction.innerHTML = contentTotalPrice;
+  }
+};
